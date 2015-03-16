b031c3fde640c1d25047c6d67cb5b5775aced885
Felix Liu <felixliu@berkeley.edu>
2015-03-15 19:33:19 -0700

    merge creating new article

--- app/models/article.rb
+++ app/models/article.rb
@@ -72,10 +72,13 @@ class Article < Content
   end
 
   def merge_data(article)
-    self.body = self.body + "\n" + article.body
-    self.comments << article.comments
-    self.save!
+    m_article = Article.new(self.attributes.except('guid','permalink'));
+    m_article.body = self.body + "\n" + article.body
+    m_article.save
+    m_article.comments << self.comments
+    m_article.comments << article.comments
     article.destroy
+    self.destroy
   end
 
 
